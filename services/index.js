8ab99dc5fee1f63171491981ab11bb026841ac06
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-01-23 16:39:24 +0800

    Update index.js

--- services/index.js
+++ services/index.js
@@ -8,13 +8,14 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect("mongodb://localhost:27017/myFirstdb");
-
+//api for signup
 app.post("/signup", (req, res) => {
   userModel
     .create(req.body)
     .then((users) => res.json(users))
     .catch((err) => res.json(err));
 });
+//api for login
 app.post("/login", async (req, res) => {
   //   const chech = userModel.findOne({ email: req.body.email });
   //   if (!checl) console.log("username not found");
