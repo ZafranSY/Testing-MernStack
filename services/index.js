5e7383797d0b6f1cc57e968e7e3284ba6e366ff1
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-01-23 13:25:15 +0800

    Update index.js

--- services/index.js
+++ services/index.js
@@ -14,6 +14,25 @@ app.post("/signup", (req, res) => {
     .then((users) => res.json(users))
     .catch((err) => res.json(err));
 });
+app.post("/login", async (req, res) => {
+  //   const chech = userModel.findOne({ email: req.body.email });
+  //   if (!checl) console.log("username not found");
+  const { email, password } = req.body;
+  userModel.findOne({ email, password }).then((user) => {
+    if (user) {
+      if (user.password == password) {
+        res.json("success");
+      } else {
+        res.json("the password or email is incorrect!");
+      }
+    } else {
+      res.json("no acconnt existed");
+    }
+  });
+
+  if (isPasswordMatch) res.render("home");
+  else res.render("wrong password");
+});
 const port = 5002;
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
