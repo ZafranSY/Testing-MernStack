05462401f7b6a4a5bcbaab279700e37407ee8cf9
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-03-10 18:45:25 +0800

    Update App.jsx

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -5,16 +5,19 @@ import LoginPage from "./Login";
 import SignupPage from "./Signup";
 import HomePage from "./home";
 
+import Testing from "./testing";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/home" element={<HomePage></HomePage>} />
-      </Routes>
+        {/* <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} /> */}
+       <Route path="/testing" element={<Testing/>} />  {/* Change this path */}
+        {/* Add a home route */}
+        <Route path="/" element={<Testing/>} />      </Routes>
     </Router>
   );
 }
